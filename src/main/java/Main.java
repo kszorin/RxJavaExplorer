b3e7cd9efa0409419b906c52db423a525d214913
kszorin <kszorin@yandex.ru>
2017-04-09 18:12:00 +0700

    added subject

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
+import rx.subjects.PublishSubject;
 
 /**
  * Created by zorin on 07.04.17.
@@ -32,7 +33,20 @@ public class Main {
         };
         observable.subscribe(observer);
 
-        System.out.println("Hello world!");
+        PublishSubject<Integer> subject = PublishSubject.create();
+        subject.subscribe(a -> {
+            a*=10;
+            System.out.println(a);
+        });
+
+        subject.onNext(1);
+        subject.onNext(2);
+        subject.onNext(3);
+        subject.onNext(4);
+        subject.onNext(5);
+        subject.onCompleted();
+        subject.onNext(4);
+        subject.onNext(5);
 
     }
 }
